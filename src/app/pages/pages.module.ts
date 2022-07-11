4ac1155a922d2f69a6cd3f5431883e35d2a6d79a
Vinicius Silva <vinilrsilv@gmail.com>
2022-07-11 08:27:32 -0300

    Adding files

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Pages
+import { RoutingModule } from './routing.module';
+
+// Components
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    DetailsComponent
+  ],
+  imports: [
+    CommonModule,
+    RoutingModule,
+    SharedModule
+  ]
+})
+export class PagesModule { }
---
+++ src/app/pages/routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+
+// Components
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class RoutingModule { }
